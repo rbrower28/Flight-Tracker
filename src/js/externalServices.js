1f246fd1df2b1ec9b706fdcceb381429391d64ec
jmattgiroux <jmattgiroux@gmail.com>
2022-05-20 13:59:35 -0600

    Created externalServices.js file, added baseURL

---
+++ src/js/externalServices.js
@@ -0,0 +1,5 @@
+// externalServices.js file
+// File's purpose is to make and receive requests for the opensky network's API
+// OpenSky REST API source: https://openskynetwork.github.io/opensky-api/rest.html
+
+const baseURL = 'https://opensky-network.org/api';
