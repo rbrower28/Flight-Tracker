9851011c2f9733972d4f3e30315a2a107e9046bd
jmattgiroux <jmattgiroux@gmail.com>
2022-05-24 17:38:20 -0600

    ran npm run format, it seems to have worked.

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,22 +1,21 @@
 module.exports = {
-    plugins: [
-      // ['@snowpack/plugin-sass' ],
-    ],
-    mount: {
-      src: '/',
+  plugins: [
+    // ['@snowpack/plugin-sass' ],
+  ],
+  mount: {
+    src: "/",
+  },
+  buildOptions: {
+    out: "build",
+    baseUrl: "/celebrity-tracker/build/",
+    clean: true,
+    htmlFragments: true,
+  },
+  experiments: {
+    optimize: {
+      // 'bundle': true,
+      minify: true,
+      target: "es2015",
     },
-    buildOptions: {
-      out: 'build',
-      baseUrl: '/celebrity-tracker/build/',
-      clean: true,
-      htmlFragments: true,
-    },
-    experiments: {
-      optimize: {
-        // 'bundle': true,
-        minify: true,
-        target: 'es2015',
-      },
-    },
-  };
-  
+  },
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // main.js file
 
 // This is just a dummy file until someone else in the group starts actually creating a main.js file or the equivalent.
-// If the main file ends up not being this one, then be sure to change line 5 of package.json from 
+// If the main file ends up not being this one, then be sure to change line 5 of package.json from
 // "main": "main.js", to instead use the js file you want.
