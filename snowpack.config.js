c7ba912bd5039f31ff8ff383127f04f885b0233c
jmattgiroux <jmattgiroux@gmail.com>
2022-05-24 17:34:53 -0600

    needed to add snowpack.config.js file to get it to work properly with npm run start

---
+++ snowpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    plugins: [
+      // ['@snowpack/plugin-sass' ],
+    ],
+    mount: {
+      src: '/',
+    },
+    buildOptions: {
+      out: 'build',
+      baseUrl: '/celebrity-tracker/build/',
+      clean: true,
+      htmlFragments: true,
+    },
+    experiments: {
+      optimize: {
+        // 'bundle': true,
+        minify: true,
+        target: 'es2015',
+      },
+    },
+  };
+  
