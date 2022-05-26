22a2443c7712845416761a5f0bba23b4199b85ef
neomart <neo.a.martin@gmail.com>
2022-05-25 17:34:18 -0700

    Contained the plane map within a class

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,8 @@
 // This is just a dummy file until someone else in the group starts actually creating a main.js file or the equivalent.
 // If the main file ends up not being this one, then be sure to change line 5 of package.json from
 // "main": "main.js", to instead use the js file you want.
+import {MapRenderer} from './mapData.js';
+let myMap = new MapRenderer('map', -114.7420, 44.0682, 5);
+myMap.addJet('N1234567', -114.7420, 44.0682);
+myMap.addJet('N1234567', -116.7420, 49.0682);
+// myMap.removeJet('N1234567');
--- src/js/mapData.js
+++ src/js/mapData.js
@@ -1,56 +1,61 @@
-var iconStyle = new ol.style.Style({
-  image: new ol.style.Icon({
-      anchor: [0.5, 46],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      src: '../images/jet-logo.png',
-      scale: 0.0625
-  })
-});
+export class MapRenderer {
+  constructor(target, long, lat, zoom) {
+    this.target = target;
+    this.long = long;
+    this.lat = lat;
+    this.zoom = zoom;
 
-// Render the map
-var map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
+    this.iconStyle = new ol.style.Style({
+      image: new ol.style.Icon({
+          anchor: [0.5, 46],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: '../images/jet-logo.png',
+          scale: 0.0625
+      })
+    });
+    
+    this.map = new ol.Map({
+      target: this.target,
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.long, this.lat]),
+        zoom: this.zoom
+      })
+    });
+  }
+
+  addJet(jetName, long, lat) {
+    var iconFeature = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.transform([long, lat], "EPSG:4326", "EPSG:3857")),
+      name: 'Null',
+    });
+  
+    iconFeature.setStyle(this.iconStyle);
+  
+    var vectorSource = new ol.source.Vector({
+      features: [iconFeature]
+    });
+  
+    var vectorLayer = new ol.layer.Vector({
+      source: vectorSource
+    });
+  
+    vectorLayer.set('name', jetName); 
+    this.map.addLayer(vectorLayer);
+  }
+
+  removeJet(jetName) {
+    var element = null;
+    this.map.getLayers().forEach(function(el) {
+      if (el.get('name') === jetName) {
+        element = el;
+      }
     })
-  ],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([-114.7420, 44.0682]),
-    zoom: 5
-  })
-});
-
-function addJet(jetName, long, lat) {
-  var iconFeature = new ol.Feature({
-    geometry: new ol.geom.Point(ol.proj.transform([long, lat], "EPSG:4326", "EPSG:3857")),
-    name: 'Null',
-  });
-
-  iconFeature.setStyle(iconStyle);
-
-  var vectorSource = new ol.source.Vector({
-    features: [iconFeature]
-  });
-
-  var vectorLayer = new ol.layer.Vector({
-    source: vectorSource
-  });
-
-  vectorLayer.set('name', jetName); 
-  map.addLayer(vectorLayer);
+    this.map.removeLayer(element);
+  }
 }
-
-function removeJet(jetName) {
-  map.getLayers().forEach(function(el) {
-    if (el.get('name') === jetName) {
-      map.removeLayer(el);
-    }
-  })
-}
-
-addJet('N123456', -114.7420, 44.0682);
-addJet('N543632', -116.7420, 45.0682);
-
-// removeJet('N543632');  
