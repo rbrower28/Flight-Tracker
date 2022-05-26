422f3ed188792d9a6672fd394e7d5ff6d67cf838
neomart <neo.a.martin@gmail.com>
2022-05-25 17:08:14 -0700

    Added functionality to add / remove jets

--- src/js/mapData.js
+++ src/js/mapData.js
@@ -1,3 +1,14 @@
+var iconStyle = new ol.style.Style({
+  image: new ol.style.Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: '../images/jet-logo.png',
+      scale: 0.0625
+  })
+});
+
+// Render the map
 var map = new ol.Map({
   target: 'map',
   layers: [
@@ -6,7 +17,40 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
-    zoom: 4
+    center: ol.proj.fromLonLat([-114.7420, 44.0682]),
+    zoom: 5
   })
 });
+
+function addJet(jetName, long, lat) {
+  var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.transform([long, lat], "EPSG:4326", "EPSG:3857")),
+    name: 'Null',
+  });
+
+  iconFeature.setStyle(iconStyle);
+
+  var vectorSource = new ol.source.Vector({
+    features: [iconFeature]
+  });
+
+  var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+  });
+
+  vectorLayer.set('name', jetName); 
+  map.addLayer(vectorLayer);
+}
+
+function removeJet(jetName) {
+  map.getLayers().forEach(function(el) {
+    if (el.get('name') === jetName) {
+      map.removeLayer(el);
+    }
+  })
+}
+
+addJet('N123456', -114.7420, 44.0682);
+addJet('N543632', -116.7420, 45.0682);
+
+// removeJet('N543632');  
