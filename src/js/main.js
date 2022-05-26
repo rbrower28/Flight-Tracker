8bd759ab14f0be5b2142fcd2031e800436858ddb
jmattgiroux <jmattgiroux@gmail.com>
2022-05-25 22:35:23 -0600

    ran lint, format, and lint again.

--- src/js/main.js
+++ src/js/main.js
@@ -21,13 +21,18 @@ const jetList = new PlaneList(source, listElement);
 jetList.init();
 
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/formdata_event
-document.getElementById("searchForm").addEventListener("submit", async (event) => {
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", async (event) => {
     event.preventDefault();
 
     let modeSCode = document.getElementById("modeSCode").value;
 
     let flightData = await getFlightDataByModeSCode(modeSCode);
 
-    myMap.addJet(flightData.states[0][0], flightData.states[0][5], flightData.states[0][6]);
-
-});
+    myMap.addJet(
+      flightData.states[0][0],
+      flightData.states[0][5],
+      flightData.states[0][6]
+    );
+  });
