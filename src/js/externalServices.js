9c8e4fe91ba0221053941a6eaac7c1f39446348f
jmattgiroux <jmattgiroux@gmail.com>
2022-05-25 20:20:28 -0600

    Changed ExternalServices.js so that it just has a function rather than a class

--- src/js/externalServices.js
+++ src/js/externalServices.js
@@ -2,27 +2,22 @@
 // File's purpose is to make and receive requests for the opensky network's API
 // OpenSky REST API source: https://openskynetwork.github.io/opensky-api/rest.html
 // OpenSky API Response Guide (ie what is what in the response): https://opensky-network.org/forum/application-programming-interface/97-opensky-api-response-states-array
-import fetch from 'node-fetch';
+// Resource used: https://itnext.io/async-and-await-in-javascript-the-extension-to-a-promise-f4e0048964ac
 
-const baseURL = 'https://opensky-network.org/api';
+// Mode S Code effectively means icao24 code; they're the same thing.
+// So when you see Mode S Code for a flight here: https://opensky-network.org/network/explorer
+// That's what's going into the function as a parameter.
 
-export default class ExternalServices {
+import fetch from "node-fetch";
+const baseURL = "https://opensky-network.org/api";
 
-    // Resource used: https://itnext.io/async-and-await-in-javascript-the-extension-to-a-promise-f4e0048964ac
-    
-    // Mode S Code effectively means icao24 code; they're the same thing.
-    // So when you see Mode S Code for a flight here: https://opensky-network.org/network/explorer
-    // That's what's going into the function as a parameter.
+export default async function getFlightDataByModeSCode(modeSCode) {
+  let data = await fetch(baseURL + "/states/all?icao24=" + modeSCode);
 
-    async getFlightDataByModeSCode(modeSCode){
-        let data = await fetch(baseURL + '/states/all?icao24=' + modeSCode);
-        
-        if (data.status == 200) {
-            let json = await data.json();
-            return json;
-        }
+  if (data.status == 200) {
+    let json = await data.json();
+    return json;
+  }
 
-        throw new Error(data.status);
-    }
-
+  throw new Error(data.status);
 }
--- test/ExternalServices.test.js
+++ test/ExternalServices.test.js
@@ -2,9 +2,7 @@
 // Resource for Mocha: https://mochajs.org/
 // Resource for assert: https://nodejs.org/api/assert.html
 import assert from 'node:assert/strict';
-import ExternalServices from '../src/js/ExternalServices.js';
-
-let externalServices = new ExternalServices;
+import getFlightDataByModeSCode from '../src/js/ExternalServices.js';
 
 // Look up flights for tests here: https://opensky-network.org/network/explorer
 // OpenSky API Response Guide (ie what is what in the response): https://opensky-network.org/forum/application-programming-interface/97-opensky-api-response-states-array
@@ -12,7 +10,7 @@ let externalServices = new ExternalServices;
 describe('ExternalServices', function () {
   describe('getFlightDataByModeSCode()', function () {
     it('Requesting c0523f (Random but specific) flight information.', async function () {
-      let data = await externalServices.getFlightDataByModeSCode('c0523f');
+      let data = await getFlightDataByModeSCode('c0523f');
       console.log(data);
       assert.ok(data, `data for flight c0523f wasn't available.`);
     });
