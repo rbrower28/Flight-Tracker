9df58b72564cb45fd0d3943bb7735ff8d295ed13
Ryan Brower <ryan.brower28@gmail.com>
2022-06-01 00:29:15 -0700

    form data validation

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,8 @@ function loadSavedFlights() {
 
       // resource: https://www.techiedelight.com/add-item-html-list-javascript/
       let node = document.createElement("li");
-      
+
+      // creates each element and populates with jet data
       let li_country = document.createElement("h4");
       li_country.textContent = jet.country;
       let li_callsign = document.createElement("p");
@@ -73,41 +74,48 @@ document
     }
 
     let modeSCode = document.getElementById("modeSCode").value;
-    document.getElementById("modeSCode").textContent = "";
 
-    let flightData = await getFlightDataByModeSCode(modeSCode);
+    if (modeSCode === "") {
+      alert("You can't track a flight without a number!")
+    } else if (flights.includes(modeSCode)) {
+      alert("This flight is already being tracked!")
+    } else {
+      let flightData = await getFlightDataByModeSCode(modeSCode);
 
-    let jet = new Jet(flightData, false);
+      let jet = new Jet(flightData, false);
 
-    myMap.addJet(jet);
+      myMap.addJet(jet);
 
-    // resource: https://www.techiedelight.com/add-item-html-list-javascript/
-    let node = document.createElement("li");
+      // resource: https://www.techiedelight.com/add-item-html-list-javascript/
+      let node = document.createElement("li");
 
-    let li_country = document.createElement("h4");
-    li_country.textContent = jet.country;
-    let li_callsign = document.createElement("p");
-    li_callsign.textContent = "Callsign: " + jet.callsign;
-    let li_modeScode = document.createElement("p");
-    let code = document.createElement("span");
-    code.textContent = jet.modeSCode
-    li_modeScode.textContent = "Mode S Code: ";
-    li_modeScode.appendChild(code);
-    let li_delete = document.createElement("button");
-    li_delete.textContent = "Remove";
-    li_delete.addEventListener("click", removeSelf);
+      // creates each element and populates with jet data
+      let li_country = document.createElement("h4");
+      li_country.textContent = jet.country;
+      let li_callsign = document.createElement("p");
+      li_callsign.textContent = "Callsign: " + jet.callsign;
+      let li_modeScode = document.createElement("p");
+      let code = document.createElement("span");
+      code.textContent = jet.modeSCode
+      li_modeScode.textContent = "Mode S Code: ";
+      li_modeScode.appendChild(code);
+      let li_delete = document.createElement("button");
+      li_delete.textContent = "Remove";
+      li_delete.addEventListener("click", removeSelf);
 
-    if (jet.country) { node.appendChild(li_country) };
-    if (jet.callsign) { node.appendChild(li_callsign) };
-    if (jet.modeSCode) { node.appendChild(li_modeScode) };
+      if (jet.country) { node.appendChild(li_country) };
+      if (jet.callsign) { node.appendChild(li_callsign) };
+      if (jet.modeSCode) { node.appendChild(li_modeScode) };
+      if (jet.modeSCode) { node.appendChild(li_delete) };
 
-    document.querySelector("#user-flights-list").appendChild(node);
+      document.querySelector("#user-flights-list").appendChild(node);
 
-    // We push the modeSCode, since we want to get recent data for the plane, not the old data stored in the jet object
-    flights.push(modeSCode);
+      // We push the modeSCode, since we want to get recent data for the plane, not the old data stored in the jet object
+      flights.push(modeSCode);
 
-    // resource: https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-    localStorage.setItem("flightsList", JSON.stringify(flights));
+      // resource: https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+      localStorage.setItem("flightsList", JSON.stringify(flights));
+    }
   });
 
 loadSavedFlights();
@@ -155,6 +163,7 @@ window.addEventListener("load", async function() {
     // add each jet to the random jet list
     let node = document.createElement("li");
 
+    // creates each element and populates with jet data
     let li_country = document.createElement("h4");
     li_country.textContent = jet.country;
     let li_callsign = document.createElement("p");
