cfd16d493777127c8e857dfc42218053882f73ad
jmattgiroux <jmattgiroux@gmail.com>
2022-06-01 22:08:01 -0600

    confirmed website works after merge in both dev and build folder; I'll deploy it again after we merge the other pull request

--- build/js/externalSource.js
+++ build/js/externalSource.js
@@ -1,10 +1,1 @@
-export default class t {
-  getPeopleData() {
-    return fetch("./data/jets.json")
-      .then((e) => e.json())
-      .then((e) => e.jets);
-  }
-  getPlaneData(e) {
-    return e;
-  }
-}
+export default class t{getPeopleData(){return fetch("./data/jets.json").then(e=>e.json()).then(e=>e.jets)}getPlaneData(e){return e}}
